e2a18ae40c77d85e02d812763467856e6c3d60a5
VickyGH <gar_vicky93@hotmail.com>
2018-01-16 15:41:45 -0600

    Tercer Commit
    Subir estaticos

---
+++ webPRE/static/MiJS1.js
@@ -0,0 +1,5 @@
+function resulta(){
+        num1 = document.getElementById("monto_asignado").value;
+        num2 = document.getElementById("monto_ejercido").value;
+        document.getElementById("resultado").value = num1 - num2;
+}
